a8038111027063666dfdd5e7987df9c25be687e6
Qayyum Suhaimi <qayyumsuhaimi10@gmail.com>
2024-07-23 01:04:45 +0800

    Add

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
+
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
